39093ac4c9553d3a1d55411cba5b911408545c5e
Bono <bono@tuta.io>
2018-09-13 21:31:42 +0400

    Move rofi audio source/sink to it's scripts

--- polybar/.config/polybar/scripts/pulseaudio.sh
+++ polybar/.config/polybar/scripts/pulseaudio.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 
-outputs() {
-    OUTPUT=$(pactl list short sinks | cut  -f 2 | rofi -dmenu -p "Output" -mesg "Select prefered output source" )
-    pacmd set-default-sink "$OUTPUT" >/dev/null 2>&1
-
-    for playing in $(pacmd list-sink-inputs | awk '$1 == "index:" {print $2}'); do
-        pacmd move-sink-input "$playing" "$OUTPUT" >/dev/null 2>&1
-    done
-}
-
-inputs() {
-    INPUT=$(pactl list short sources | cut  -f 2 | grep input | rofi -dmenu -p "Output" -mesg "Select prefered input source" )
-    pacmd set-default-source "$INPUT" >/dev/null 2>&1
-
-    for recording in $(pacmd list-source-outputs | awk '$1 == "index:" {print $2}'); do
-        pacmd move-source-output "$recording" "$INPUT" >/dev/null 2>&1
-    done
-}
-
 volume_up() {
     pactl set-sink-volume @DEFAULT_SINK@ +3%
 }
@@ -47,7 +29,7 @@ get_default_sink() {
 }
 
 output_volume() {
-     pacmd list-sinks | awk '/^\s+name: /{indefault = $2 == "'"<$(get_default_sink)>"'"}
+    pacmd list-sinks | awk '/^\s+name: /{indefault = $2 == "'"<$(get_default_sink)>"'"}
              /^\s+muted: / && indefault {muted=$2}
              /^\s+volume: / && indefault {volume=$5}
              END { print muted=="no"?"  "volume:"      " }'
@@ -81,12 +63,6 @@ input_volume_listener() {
 }
 
 case "$1" in
-    --output)
-        outputs
-    ;;
-    --input)
-        inputs
-    ;;
     --mute)
         mute
     ;;
