ac8fe4cea5717aebdb88fb482bef6004098bc418
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2021-02-11 07:36:32 +0400

    Drop duplicates from zsh history

--- packages/zsh/.zsh/history.zsh
+++ packages/zsh/.zsh/history.zsh
@@ -1,9 +1,12 @@
 setopt histignoredups
+setopt histignorealldups
+setopt histfindnodups
 setopt append_history
 setopt hist_expire_dups_first
 setopt hist_ignore_space
 setopt inc_append_history
 setopt share_history
+setopt histreduceblanks
 
 HISTFILE="$HOME/.zhistory"
 HISTSIZE=10000000
